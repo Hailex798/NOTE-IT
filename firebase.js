b81053a33b1951cb6df7c45532d8ed3a2a037226
Hailex <kshitij.singh.bisht.174sis@gmail.com>
2023-12-16 20:17:29 +0530

    Updated the DB: Full & Final time -> eternity

--- firebase.js
+++ firebase.js
@@ -2,15 +2,19 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore, collection } from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAXHV7chfUZfexzw9EVn_lTjIF0KfgFLvU",
-  authDomain: "my-notes-bdbc9.firebaseapp.com",
-  projectId: "my-notes-bdbc9",
-  storageBucket: "my-notes-bdbc9.appspot.com",
-  messagingSenderId: "87987197603",
-  appId: "1:87987197603:web:425846783f357b74041116",
-  measurementId: "G-WYVFKTY2CY"
+  apiKey: "AIzaSyCziE8Q0r6TaoQLrlmbVl0Asg8BAYyFKyI",
+  authDomain: "my-notes-d3556.firebaseapp.com",
+  projectId: "my-notes-d3556",
+  storageBucket: "my-notes-d3556.appspot.com",
+  messagingSenderId: "1016085207610",
+  appId: "1:1016085207610:web:78eb24672d2719c8953303",
+  measurementId: "G-C4SGDHWDZH"
 };
 
 // Initialize Firebase
